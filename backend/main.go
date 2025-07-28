517d5812a98886c0188a2b5114e79aa22b4bdc89
jkuya <kuyajesee@proton.me>
2025-07-28 03:41:48 +0300

    feat: initialize the entry point

---
+++ backend/main.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"log"
+	"net/http"
+
+	"github.com/jesee-kuya/marineshop/pkg/db"
+	"github.com/jesee-kuya/marineshop/pkg/handler"
+	"github.com/jesee-kuya/marineshop/pkg/repository"
+)
+
+func main() {
+	db, err := db.Init()
+	if err != nil {
+		log.Default().Println(err)
+	}
+
+	app := &handler.App{
+		Query: &repository.Query{
+			Db: db,
+		},
+	}
+
+	server := http.Server{
+		Addr:    ":8080",
+		Handler: app.Routes(),
+	}
+
+	go func() {
+		if err := server.ListenAndServe(); err != nil {
+			log.Default().Println(err)
+			return
+		}
+	}()
+
+	log.Default().Printf("Server started on port %s", server.Addr)
+}
