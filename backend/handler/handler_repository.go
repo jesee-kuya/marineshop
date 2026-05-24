e882567e868e501ef643b785d012c835ceedbb65
jesee kuya <kuyajesee@proton.me>
2026-05-24 15:06:40 +0300

    feat: define auth, buyer, seller and admin handler interfaces

---
+++ backend/handler/handler_repository.go
@@ -0,0 +1,41 @@
+package handler
+
+type AuthHandler interface {
+	Login(c *gin.Context)
+	Signup(c *gin.Context)
+	Logout(c *gin.Context)
+	ChangePassword(c *gin.Context)
+	ResetPassword(c *gin.Context)
+}
+
+type SellerHandler interface {
+	CollectkYC(c *gin.Context)
+	SetUpShop(c *gin.Context)
+	SetUpPayment(c *gin.Context)
+	CreateProduct(c *gin.Context)
+	UpdateProduct(c *gin.Context)
+	DeleteProduct(c *gin.Context)
+	GetMyProducts(c *gin.Context)
+	Withdraw(c *gin.Context)
+	TransactionHistory(c *gin.Context)
+	OrderManagement(c *gin.Context)
+	Analytics(c *gin.Context)
+	Profile(c *gin.Context)
+}
+
+type BuyerHandler interface {
+	Recomendations(c *gin.Context)
+	GetProducts(c *gin.Context)
+	Profile(c *gin.Context)
+	AddCartItem(c *gin.Context)
+	GetCartItems(c *gin.Context)
+	RemoveCartItem(c *gin.Context)
+	Checkout(c *gin.Context)
+	TransactionHistory(c *gin.Context)
+	OrderManagement(c *gin.Context)
+}  
+type AdminHandler interface {
+	GETKYC(c *gin.Context)
+	ApproveKYC(c *gin.Context)
+	RejectKYC(c *gin.Context)
+}
