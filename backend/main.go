871829b81f07b1a07de80ee766d0b13e47d326c6
jesee kuya <kuyajesee@proton.me>
2026-05-24 10:44:17 +0300

    remove: delete the existing setup

--- backend/main.go
+++ backend/main.go
@@ -4,6 +4,8 @@ import (
 	"log"
 	"net/http"
 
+	"github.com/gin-gonic/gin"
+
 	database "github.com/jesee-kuya/marineshop/pkg/db"
 	"github.com/jesee-kuya/marineshop/pkg/handler"
 	"github.com/jesee-kuya/marineshop/pkg/model"
@@ -11,30 +13,4 @@ import (
 )
 
 func main() {
-	db, err := database.Init()
-	if err != nil {
-		log.Default().Println(err)
-	}
-
-	app := handler.App{
-		Queries: repository.Query{
-			Db: db,
-		},
-		User: &model.User{},
-	}
-
-	server := http.Server{
-		Addr:    ":8080",
-		Handler: app.Routes(),
-	}
-
-	go func() {
-		if err := server.ListenAndServe(); err != nil {
-			log.Default().Println(err)
-			return
-		}
-	}()
-
-	log.Default().Printf("Server started on port %s", server.Addr)
-	select {}
 }
--- backend/pkg/db/sqlite/000005_create_rating_triggers.down.sql
+++
@@ -1,3 +0,0 @@
-DROP TRIGGER IF EXISTS update_rating_after_insert;
-DROP TRIGGER IF EXISTS update_rating_after_update;
-DROP TRIGGER IF EXISTS update_rating_after_delete;
