25d36add87d8effad84f9a739e42172fbda5128c
jesee kuya <kuyajesee@proton.me>
2026-05-25 11:45:23 +0300

    feat: write migrations for users table

---
+++ backend/migrations/114325062026_user.up.sql
@@ -0,0 +1,12 @@
+CREATE TABLE IF NOT EXISTS users (
+    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+
+    username VARCHAR(255) NOT NULL UNIQUE,
+    email VARCHAR(255) NOT NULL UNIQUE,
+    password TEXT NOT NULL,
+
+    role VARCHAR(50) NOT NULL DEFAULT 'buyer',
+
+    created_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
+    updated_at TIMESTAMPTZ NOT NULL DEFAULT NOW()
+);
