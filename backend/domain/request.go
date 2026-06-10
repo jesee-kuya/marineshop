171a02245dbfa2f9d1cf4ae951805030a0fdefde
jesee kuya <kuyajesee@proton.me>
2026-06-10 15:37:28 +0300

    feat: declare auth request types

---
+++ backend/domain/request.go
@@ -0,0 +1,12 @@
+package domain
+
+type SignupRequest struct {
+	Username string `json:"username" binding:"required,min=3,max=255"`
+	Email    string `json:"email" binding:"required,email"`
+	Password string `json:"password" binding:"required,min=8"`
+}
+
+type LoginRequest struct {
+	Email    string `json:"email" binding:"required,email"`
+	Password string `json:"password" binding:"required"`
+}
