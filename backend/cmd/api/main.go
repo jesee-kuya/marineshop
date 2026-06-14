2c31f2fb603e005b608b6cf60ef34f61ee0e98cd
jesee kuya <kuyajesee@proton.me>
2026-06-14 16:45:17 +0300

    feat: add graceful shutdown by blocking on OS interrupt and SIGTERM signals

--- backend/cmd/api/main.go
+++ backend/cmd/api/main.go
@@ -3,6 +3,9 @@ package main
 import (
 	"fmt"
 	"log"
+	"os"
+	"os/signal"
+	"syscall"
 
 	"github.com/jesee-kuya/marineshop/config"
 	"github.com/jesee-kuya/marineshop/database"
@@ -43,5 +46,10 @@ func main() {
 			log.Fatalf("Failed to start server: %v", err)
 		}
 	}()
+
+	quit := make(chan os.Signal, 1)
+	signal.Notify(quit, os.Interrupt, syscall.SIGTERM)
+
+	<-quit
 
 }
