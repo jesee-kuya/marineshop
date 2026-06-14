48c6768adbb53363fc0d77ca19e6840711255339
jesee kuya <kuyajesee@proton.me>
2026-06-14 16:44:58 +0300

    refactor: change ChangePassword and ResetPassword receivers from Authentication to Marineshop

--- backend/handler/password_management.go
+++ backend/handler/password_management.go
@@ -6,10 +6,10 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
-func (h *Authentication) ChangePassword(c *gin.Context) {
+func (shop *Marineshop) ChangePassword(c *gin.Context) {
 	c.JSON(http.StatusNotImplemented, gin.H{"message": "not implemented"})
 }
 
-func (h *Authentication) ResetPassword(c *gin.Context) {
+func (shop *Marineshop) ResetPassword(c *gin.Context) {
 	c.JSON(http.StatusNotImplemented, gin.H{"message": "not implemented"})
 }
