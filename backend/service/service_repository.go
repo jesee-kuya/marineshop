4b4c45ef62a696692de23ad2a489d6fe918c6592
jesee kuya <kuyajesee@proton.me>
2026-06-17 16:36:14 +0300

    feat: add Login, ChangePassword, and ResetPassword to AuthService interface

--- backend/service/service_repository.go
+++ backend/service/service_repository.go
@@ -3,9 +3,13 @@ package service
 import (
 	"context"
 
+	"github.com/google/uuid"
 	"github.com/jesee-kuya/marineshop/domain"
 )
 
 type AuthService interface {
 	Signup(ctx context.Context, req *domain.SignupRequest) (*domain.AuthResponse, error)
+	Login(ctx context.Context, req *domain.LoginRequest) (*domain.AuthResponse, error)
+	ChangePassword(ctx context.Context, userID uuid.UUID, req *domain.ChangePasswordRequest) error
+	ResetPassword(ctx context.Context, req *domain.ResetPasswordRequest) error
 }
