536e61fb94009a3d54c975b2b31a74bf67ed1d6f
jesee kuya <kuyajesee@proton.me>
2026-06-17 16:36:25 +0300

    feat: implement Login handler

--- backend/handler/login.go
+++ backend/handler/login.go
@@ -1,11 +1,31 @@
 package handler
 
 import (
+	"errors"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
+	"github.com/jesee-kuya/marineshop/domain"
 )
 
 func (shop *Marineshop) Login(c *gin.Context) {
-	c.JSON(http.StatusNotImplemented, gin.H{"message": "not implemented"})
+	var req domain.LoginRequest
+	if err := c.ShouldBindJSON(&req); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	res, err := shop.AuthService.Login(c.Request.Context(), &req)
+	if err != nil {
+		if errors.Is(err, domain.ErrInvalidCredentials) {
+			c.JSON(http.StatusUnauthorized, gin.H{"error": err.Error()})
+			return
+		} else if errors.Is(err, domain.ErrUserNotFound) {
+			c.JSON(http.StatusNotFound, gin.H{"error": err.Error()})
+		}
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "failed to login"})
+		return
+	}
+
+	c.JSON(http.StatusOK, res)
 }
