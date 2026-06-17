8c6aa09f4750c33025fc053a083ea2f20f174d5e
jesee kuya <kuyajesee@proton.me>
2026-06-17 16:36:22 +0300

    feat: implement ChangePassword and ResetPassword business logic on the Auth service

---
+++ backend/service/password_management.go
@@ -0,0 +1,49 @@
+package service
+
+import (
+	"context"
+	"database/sql"
+	"errors"
+
+	"github.com/google/uuid"
+	"github.com/jesee-kuya/marineshop/domain"
+	"github.com/jesee-kuya/marineshop/utils"
+)
+
+func (s *Auth) ChangePassword(ctx context.Context, userID uuid.UUID, req *domain.ChangePasswordRequest) error {
+	user, err := s.UserRepo.FindByID(ctx, userID)
+	if err != nil {
+		if errors.Is(err, sql.ErrNoRows) {
+			return domain.ErrUserNotFound
+		}
+		return err
+	}
+
+	if !utils.CheckPassword(req.OldPassword, user.Password) {
+		return domain.ErrInvalidCredentials
+	}
+
+	hashed, err := utils.HashPassword(req.NewPassword)
+	if err != nil {
+		return err
+	}
+
+	return s.UserRepo.UpdatePassword(ctx, userID, hashed)
+}
+
+func (s *Auth) ResetPassword(ctx context.Context, req *domain.ResetPasswordRequest) error {
+	user, err := s.UserRepo.FindByEmail(ctx, req.Email)
+	if err != nil {
+		if errors.Is(err, sql.ErrNoRows) {
+			return domain.ErrUserNotFound
+		}
+		return err
+	}
+
+	hashed, err := utils.HashPassword(req.NewPassword)
+	if err != nil {
+		return err
+	}
+
+	return s.UserRepo.UpdatePassword(ctx, user.ID, hashed)
+}
