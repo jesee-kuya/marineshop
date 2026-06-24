8577e514a15c06e79ad8428f72f4258cd47459dc
jesee kuya <kuyajesee@proton.me>
2026-06-24 06:57:28 +0300

    Feat: define payments errors

--- backend/domain/errors.go
+++ backend/domain/errors.go
@@ -3,10 +3,11 @@ package domain
 import "errors"
 
 var (
-	ErrEmailInUse              = errors.New("email already in use")
-	ErrInvalidCredentials      = errors.New("invalid email or password")
-	ErrUserNotFound            = errors.New("user not found")
-	ErrKYCAlreadyExists        = errors.New("KYC already submitted")
-	ErrSellerKYCNotFound       = errors.New("seller KYC not found, please complete personal KYC first")
+	ErrEmailInUse               = errors.New("email already in use")
+	ErrInvalidCredentials       = errors.New("invalid email or password")
+	ErrUserNotFound             = errors.New("user not found")
+	ErrKYCAlreadyExists         = errors.New("KYC already submitted")
+	ErrSellerKYCNotFound        = errors.New("seller KYC not found, please complete personal KYC first")
 	ErrBusinessKYCAlreadyExists = errors.New("business KYC already submitted")
+	ErrInvalidPaymentDetails    = errors.New("invalid payment details")
 )
