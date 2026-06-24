1c3d8fda0e4e444f4352e438edc71a4735be20c6
jesee kuya <kuyajesee@proton.me>
2026-06-24 05:41:38 +0300

    Feat: define the CollectKYCRequest struct type

--- backend/domain/request.go
+++ backend/domain/request.go
@@ -20,3 +20,12 @@ type ResetPasswordRequest struct {
 	Email       string `json:"email" binding:"required,email"`
 	NewPassword string `json:"new_password" binding:"required,min=8"`
 }
+
+type CollectKYCRequest struct {
+	FullName           string `json:"full_name" binding:"required"`
+	PhoneNumber        string `json:"phone_number" binding:"required"`
+	NationalID         string `json:"national_id" binding:"required"`
+	NationalIDDocument string `json:"national_id_document" binding:"required"`
+	Selfie             string `json:"selfie" binding:"required"`
+	Location           string `json:"location" binding:"required"`
+}
