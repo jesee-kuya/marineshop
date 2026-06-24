890fcf601bada8cc072293c13341a018009fe8c3
jesee kuya <kuyajesee@proton.me>
2026-06-24 05:40:22 +0300

    feat: add the ErrKYCAlreadyExists error'

--- backend/domain/errors.go
+++ backend/domain/errors.go
@@ -6,4 +6,5 @@ var (
 	ErrEmailInUse         = errors.New("email already in use")
 	ErrInvalidCredentials = errors.New("invalid email or password")
 	ErrUserNotFound       = errors.New("user not found")
+	ErrKYCAlreadyExists   = errors.New("KYC already submitted")
 )
