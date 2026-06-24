1972c3c0afeb7504c5d20b13b8bffee67569abfb
jesee kuya <kuyajesee@proton.me>
2026-06-24 06:58:36 +0300

    Feat: define the SetUpPaymentRequest struct

--- backend/domain/request.go
+++ backend/domain/request.go
@@ -35,3 +35,15 @@ type SetUpShopRequest struct {
 	DocumentType string `json:"document_type" binding:"required,oneof=permit certificate incorporation_letter"`
 	Document     string `json:"document" binding:"required"`
 }
+
+type SetUpPaymentRequest struct {
+	WalletType    string `json:"wallet_type" binding:"required,oneof=mpesa airtel_money bank crypto"`
+	AccountName   string `json:"account_name" binding:"required"`
+	PhoneNumber   string `json:"phone_number"`
+	BankName      string `json:"bank_name"`
+	BankCode      string `json:"bank_code"`
+	AccountNumber string `json:"account_number"`
+	CryptoAddress string `json:"crypto_address"`
+	CryptoNetwork string `json:"crypto_network"`
+	IsDefault     bool   `json:"is_default"`
+}
